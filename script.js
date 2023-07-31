7433cfaf138685eda5554bc2f3fd7528246a2cdf
ankita90singh <138467717+ankita90singh@users.noreply.github.com>
2023-07-31 14:34:57 +0530

    Add files via upload
    level1

---
+++ script.js
@@ -0,0 +1,254 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gsap.from("#section",{
+    y:25,
+   
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: "#section",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+
+
+gsap.from("#section1",{
+    y:25,
+   scale: 1,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: "#section1",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+gsap.from(".sectiong ",{
+    y:25,
+  
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: ".sectiong",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+
+gsap.from(".section1g",{
+    y:25,
+   scale: 1,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: ".section1g",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+
+gsap.from("#part",{
+    y:25,
+   scale: .3,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: "#part",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+gsap.from("#part1",{
+    y:25,
+   scale: .3,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: "#part1",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+gsap.from(".part1",{
+    y:25,
+   scale: .3,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: ".part1",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+
+gsap.from(".part2",{
+    y:25,
+   scale: .3,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: ".part2",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+gsap.from(".part3",{
+    y:25,
+   scale: .3,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: ".part3",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+gsap.from(".part4",{
+    y:25,
+   scale: .3,
+    opacity: 0,
+    stagger: 1,
+    duration: 1,
+    delay:1,
+      scrollTrigger: {
+      trigger: ".part4",
+      scroller: "body",
+      // markers:true,
+      start: "top 30%",
+      end: "top 45%",
+      scrub: 1,
+    },
+
+
+}
+
+)
+
+
+gsap.from("#partion",{
+    y:25,
+   scale: .1,
+    opacity: 0,
+     rotation: 180, x: 15, yPercent: 60,
+    stagger: 1,
+    duration: 1,
+    delay:.5,
+      scrollTrigger: {
+      trigger: "#partion",
+      scroller: "body",
+      // markers:true,
+      start: "top 100%",
+      end: "top 95%",
+      scrub: 1,
+    },
+
+
+}
+
+)
